b03e568c836a3db61f808b7368c1d87e81653240
blackkpanther <59194822+blackkpanther@users.noreply.github.com>
2019-12-29 15:49:43 +0200

    Add files via upload

---
+++ BL/BL_imp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using BE;
+
+namespace BL
+{
+    class BL_imp
+    {
+    }
+}
---
+++ BL/Ibl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using BE;
+
+namespace BL
+{
+    class Ibl
+    {
+        void AddGuestRequest(GuestRequest request);
+        void UpdateGuestRequest(ref GuestRequest request, Enums.Status status);
+        void AddHostingUnit(HostingUnit unit);
+        void UpdateHostingUnit(ref HostingUnit unit, bool[,] diary);
+        void DeleteHostingUnit(HostingUnit unit);
+        void AddOrder(Order order);
+        void UpdateOrder(ref Order order, Enums.Status status);
+        List<HostingUnit> GetHostingUnitList();
+        List<GuestRequest> GetGuestRequestList();
+        List<Order> GetOrderList();
+        List<BankBranch> GetBankBranchList();
+        GuestRequest CheckGuestRequest(int key);
+        HostingUnit CheckHostingUnit(int key);
+        Order CheckOrder(int key);
+    }
+}
