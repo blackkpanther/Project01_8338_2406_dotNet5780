28a4bd7aeb8cc442f9aea77e1009df5254768bf5
blackkpanther <59194822+blackkpanther@users.noreply.github.com>
2019-12-29 15:21:23 +0200

    Add files via upload

---
+++ BankBranch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class BankBranch
+    {
+        private int bankNumber;
+        private Enums.BankName bankName;
+        private int branchNumber;
+        private string branchAddress;
+        private Enums.SubArea branchCity;
+     
+        //properties:
+
+        public int BankNumber
+        {
+            get { return bankNumber; }
+            set { bankNumber = value; }
+        }
+        public Enums.BankName BankName
+        {
+            get { return bankName; }
+            set { bankName = value; }
+        }
+        public int BranchNumber
+        {
+            get { return branchNumber; }
+            set { branchNumber = value; }
+        }
+        public string BranchAddress
+        {
+            get { return branchAddress; }
+            set { branchAddress = value; }
+        }
+        public Enums.SubArea BranchCity
+        {
+            get { return branchCity; }
+            set { branchCity = value; }
+        }
+        //printing method
+        public override string ToString()
+        {
+            return "Bank account no.: "+this.bankNumber + " Bank name: " + this.bankName + " Branch no.: " + this.branchNumber + " Branch address: " + this.branchAddress + " City: " + this.branchCity;
+        }
+        //constructor
+        public BankBranch() { }
+    }
+}
---
+++ HostingUnit.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BE
+{
+   public class HostingUnit
+    {
+        private int hostingUnitKey;
+        private Host owner;
+        private string hostringUnitName;
+        private bool[,] diary;
+        private Enums.SubArea subArea;// *
+
+        //properties:
+
+        public int HostingUnitKey
+        {
+            get { return hostingUnitKey; }
+            set { hostingUnitKey = value; }
+        }
+        public Host Owner
+        {
+            get;
+            set;
+        }
+        public string HostringUnitName
+        {
+            get { return hostringUnitName; }
+            set { hostringUnitName = value; }
+        }
+        public bool[,] Diary
+        {
+            get { return diary; }
+            set { diary = value; }
+        }
+        public Enums.SubArea SubArea
+        {
+            get { return subArea; }
+            set { subArea = value; }
+        }
+
+        //printing method
+        public override string ToString()
+        {
+            return "Hosting unit key: " + this.hostingUnitKey + " Owner: " + this.owner.PrivateName + " " + this.owner.FamilyName + " Hosting unit name: " + this.hostringUnitName + " Diary: " + this.diary;
+        }
+        public HostingUnit() { }
+    }
+}
