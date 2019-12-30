57797eceb787bf969926883e1e2b10188d078827
blackkpanther <59194822+blackkpanther@users.noreply.github.com>
2019-12-30 11:19:35 +0200

    https://github.com/blackkpanther/Project01_8338_2406_dotNet5780

--- BL/Ibl.cs
+++ BL/Ibl.cs
@@ -9,8 +9,10 @@ namespace BL
 {
     public interface Ibl
     {
+        #region GuestRequest
         void AddGuestRequest(GuestRequest request);
         void UpdateGuestRequest(ref GuestRequest request, Enums.Status status);
+        #endregion
         void AddHostingUnit(HostingUnit unit);
         void UpdateHostingUnit(ref HostingUnit unit, bool[,] diary);
         void DeleteHostingUnit(HostingUnit unit);
@@ -23,5 +25,13 @@ namespace BL
         GuestRequest CheckGuestRequest(int key);
         HostingUnit CheckHostingUnit(int key);
         Order CheckOrder(int key);
+        List<HostingUnit> AvailableHostingUnits(DateTime date, int n);
+        int NumberOfDays(DateTime date1, DateTime date2 = 0);
+        List<Order> NumberOfOrders(int days);
+        List<GuestRequest> Requests();
+        int NumberOfInvites(GuestRequest request);
+        int NumberOfInvites(HostingUnit unit);
+
+
     }
 }
